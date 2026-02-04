4f56a363bd83481bdcae18185b96e17b4a7f2889
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-03 20:17:36 -0500

    SQL 50 - 577 Employee Bonus

---
+++ challenge10.sql
@@ -0,0 +1,18 @@
+-- SQL 50 - 577 Employee Bonus
+/* Write a solution to report the name and bonus amount of each employee who satisfies either of the following:
+
+  1. The employee has a bonus less than 1000.
+  2. The employee did not get any bonus.
+
+Return the result table in any order.
+*/
+
+SELECT 
+    e.name, 
+    b.bonus
+FROM
+    employee e left join bonus b on e.empId = b.empId
+WHERE
+    b.bonus < 1000 or e.empId not in (select empId from bonus)
+ORDER BY
+    e.name;
