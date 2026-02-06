8403c61003c3789a81e5e405b34754983a8d6021
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-05 21:36:40 -0500

    SQL 50 - Not Boring Movies

---
+++ challenge12.sql
@@ -0,0 +1,12 @@
+-- SQL 50 - Not Boring Movies
+/* 
+  Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+  Return the result table ordered by rating in descending order.
+*/
+
+select id, movie, description, rating from Cinema Where id in (
+    select case
+        when id % 2 != 0 and description != 'boring' then id
+    end as Odd_Id
+    from Cinema
+) order by id desc;
