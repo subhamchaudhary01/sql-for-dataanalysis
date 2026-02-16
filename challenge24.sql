567c4743a540c8c9d17170ecfc1e8f453113405f
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-16 16:01:57 -0500

    Challenge 24

---
+++ challenge24.sql
@@ -0,0 +1,12 @@
+/*
+there is a table with daily weather data over the last 6 months of 2020, including the maximum, minimum and average temperatures. Write a query that gives month, monthly maximum, monthly minimum, monthly average temperatures for six months. Round the average to nearest integer.
+*/
+
+SELECT
+    MONTH(record_date) AS month,
+    MAX(CASE WHEN data_type = 'max' THEN data_value END) AS monthly_max,
+    MIN(CASE WHEN data_type = 'min' THEN data_value END) AS monthly_min,
+    ROUND(AVG(CASE WHEN data_type = 'avg' THEN data_value END)) AS monthly_avg
+FROM temperature_records
+GROUP BY MONTH(record_date)
+ORDER BY month;
