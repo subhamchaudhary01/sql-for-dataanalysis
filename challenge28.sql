7bb1787c36a870f18263f65dd2026c4fc4b9deae
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-20 13:56:41 -0500

    Window Function (Partion and Order by)

---
+++ challenge28.sql
@@ -0,0 +1,14 @@
+-- using partition by & order by
+-- Month till Date (MTD)
+
+with a as (select
+	o.order_date,
+    round(sum(od.quantity * p.price), 2) as Sales
+from
+	orders o join order_details od on o.order_id = od.order_id join pizzas p on p.pizza_id = od.pizza_id
+group by 
+	o.order_date)
+
+select *,
+round(sum(Sales) over (order by order_date), 2) as Cumulative_Sales, 
+round(sum(Sales) over (partition by month(order_date) order by order_date), 2) as MTD from a;
