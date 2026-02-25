378024054d665fbc60036695d7a99554064630fe
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-24 20:33:16 -0500

    update

--- challenge21.sql
+++ challenge21.sql
@@ -1,8 +1,9 @@
 -- SQL 50 - Immediate Food Delivery II
 /*
   If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled. The first order of a customer is the order with the earliest order date that the customer made. It is guaranteed that a customer has precisely one first order.
+
   Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
-*/- 
+*/
 
 SELECT 
 ROUND(
