8cdc86c56cb1b543b7b1138a51209b21694614f8
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-24 20:37:18 -0500

    update

--- challenge5.sql
+++ challenge5.sql
@@ -1,4 +1,6 @@
-/* Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15. */
+/* 
+Leetcode SQL 50 Basic problem
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15. */
 
 SELECT
     tweet_id
