48e5106ef0fb2ce20878c847d818830be3d5aa85
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-25 21:59:44 -0500

    SQL 50 - Classes with at least 5 students

---
+++ challenge31.sql
@@ -0,0 +1,18 @@
+-- SQL 50 - Classes with at least 5 students
+/*
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+Write a solution to find all the classes that have at least five students.
+Return the result table in any order.
+*/
+
+select 
+  class 
+from 
+  courses 
+group by 
+  class 
+having 
+  count(student) >= 5 
+order by 
+  class asc;
