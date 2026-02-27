e783b5f28de6443a8fbb795fd813128689b27ce1
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-27 14:56:11 -0500

    Customers who bought all products

---
+++ challenge36.sql
@@ -0,0 +1,11 @@
+-- SQL 50 - Customers who bought all products
+/*
+  Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+*/
+
+SELECT 
+    customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = 
+       (SELECT COUNT(*) FROM Product);
