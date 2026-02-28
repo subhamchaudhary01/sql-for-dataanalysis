036a344448e1c482dd2395d280e4e3a32a8e629a
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-28 14:26:20 -0500

    Updating the query

--- challenge37.sql
+++ challenge37.sql
@@ -3,9 +3,12 @@
   ind the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company. When a manager leaves the company, their information is deleted from the Employees table, but the reports still have their manager_id set to the manager that left.
 */
 
-select
-    employee_id
-from 
-    employees
-where 
-    employee_id in (select manager_id from employees where salary < 30000);
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000
+  AND manager_id IS NOT NULL
+  AND manager_id NOT IN (
+        SELECT employee_id
+        FROM Employees
+  )
+ORDER BY employee_id;
