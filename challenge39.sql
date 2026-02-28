b3dadab96ac697fc3e1672405f405bce5f08a12b
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-02-28 15:06:11 -0500

    Adding comment

--- challenge39.sql
+++ challenge39.sql
@@ -1,4 +1,4 @@
--- Primary Department for Each Employee
+-- SQL 50 - 1789 Primary Department for Each Employee
 /*
   Employees can belong to multiple departments. When the employee joins other departments, they need to decide which department is their primary department. Note that when an employee belongs to only one department, their primary column is 'N'.
   Write a solution to report all the employees with their primary department. For employees who belong to one department, report their only department.
