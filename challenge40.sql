867cf7716e674c9a96197f462a4c194b14090ddb
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-01 14:02:56 -0500

    SQL 50 - Triangle Judgement

---
+++ challenge40.sql
@@ -0,0 +1,20 @@
+-- SQL 50 - Triangle Judgement
+/* 
+  In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+Report for every three line segments whether they can form a triangle.
+Return the result table in any order.
+*/
+
+SELECT 
+    x,
+    y,
+    z,
+    CASE 
+        WHEN x + y > z 
+         AND x + z > y 
+         AND y + z > x 
+        THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
