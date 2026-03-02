20b0bca6481322830cb7b39f495dc0eaa2f94226
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-02 14:57:54 -0500

    Update Count Salary Categories

--- challenge44.sql
+++ challenge44.sql
@@ -1,6 +1,12 @@
 -- SQL 50 - Count Salary Categories
 /*
+  Write a solution to calculate the number of bank accounts for each salary category. The salary categories are:
 
+    "Low Salary": All the salaries strictly less than $20000.
+    "Average Salary": All the salaries in the inclusive range [$20000, $50000].
+    "High Salary": All the salaries strictly greater than $50000.
+
+The result table must contain all three categories. If there are no accounts in a category, return 0.
 */
 
 SELECT 'Low Salary' AS category,
