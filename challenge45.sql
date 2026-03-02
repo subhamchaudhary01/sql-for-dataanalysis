3c96cfeff0e04e0a6a15545f412f2864a72bae27
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-02 14:59:53 -0500

    SQL 50 - Exchange Seats

---
+++ challenge45.sql
@@ -0,0 +1,17 @@
+-- SQL 50 - Exchange Seats
+/*
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+Return the result table ordered by id in ascending order.
+*/
+
+SELECT 
+    CASE
+        WHEN id % 2 = 1 AND id != (SELECT MAX(id) FROM Seat)
+            THEN id + 1
+        WHEN id % 2 = 0
+            THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
