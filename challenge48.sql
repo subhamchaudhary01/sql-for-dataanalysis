5b7807acee6ca3d3cd3d4a79873fc201e0a71f04
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-03 19:13:57 -0500

    SQL 50 - 602 Friend Requests II: Who has the most friends

---
+++ challenge48.sql
@@ -0,0 +1,17 @@
+-- SQL 50 - 602 Friend Requests II: Who Has the Most Friends
+/*
+Write a solution to find the people who have the most friends and the most friends number.
+The test cases are generated so that only one person has the most friends.
+*/
+
+SELECT 
+    id,
+    COUNT(*) AS num
+FROM (
+        SELECT requester_id AS id FROM RequestAccepted
+        UNION ALL
+        SELECT accepter_id FROM RequestAccepted
+     ) t
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
