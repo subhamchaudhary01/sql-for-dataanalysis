c88ebcf6e09ef5656f33e6d866b24c3ad99fc352
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-04 23:17:38 -0500

    Emails Valid update query

--- challenge57.sql
+++ challenge57.sql
@@ -3,6 +3,6 @@
   Write a solution to find the users who have valid emails. A valid e-mail has a prefix name and a domain where: The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter. The domain is '@leetcode.com'.
 */
 
-SELECT *
+SELECT user_id, name, mail
 FROM Users
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$';
+WHERE REGEXP_LIKE(mail, '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$', 'c');
