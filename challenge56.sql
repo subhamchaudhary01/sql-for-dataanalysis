62b11285d66366af920780305bc2dc6c1b08bd7b
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-04 23:12:56 -0500

    SQL 50 - List the products ordered in a paeriod

---
+++ challenge56.sql
@@ -0,0 +1,15 @@
+-- SQL 50 - List the products ordered in a period
+/*
+  Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+*/
+
+SELECT 
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o
+    ON p.product_id = o.product_id
+WHERE o.order_date >= '2020-02-01'
+  AND o.order_date <= '2020-02-29'
+GROUP BY p.product_name
+HAVING SUM(o.unit) >= 100;
