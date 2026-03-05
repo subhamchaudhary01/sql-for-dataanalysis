8bc2eeeacee973d4fba434b2b66ec9eab0d7de17
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-04 23:10:53 -0500

    SQL 50 - Group sold products by the date

---
+++ challenge55.sql
@@ -0,0 +1,12 @@
+-- SQL 50 - Group Sold Products By The Date
+/*
+  Write a solution to find for each date the number of different products sold and their names. The sold products names for each date should be sorted lexicographically. Return the result table ordered by sell_date.
+*/
+
+SELECT 
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;
