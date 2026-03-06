fa141871930f5b1afe5a26e42205592ee32dc3eb
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-05 23:37:08 -0500

    update

--- challenge51.sql
+++ challenge51.sql
@@ -1,7 +1,6 @@
 -- SQL 50 - Fix Names in a Table
 /*
-user_id is the primary key (column with unique values) for this table.
-This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters
+
 Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
 Return the result table ordered by user_id.
 */
