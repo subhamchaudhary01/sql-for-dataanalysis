bcc991477b6891d28c198ccd53ab7500f61c6c92
11subhamchaudhary <11subhamchaudhary@gmail.com>
2026-03-06 23:57:53 -0500

    update

--- challenge11.sql
+++ challenge11.sql
@@ -3,5 +3,13 @@
 Write a solution to find the number of times each student attended each exam.
 Return the result table ordered by student_id and subject_name.
 */
-
+  select
+    count(subject)
+  from
+    examination
+  group by 
+    student_id, subject
+  order by
+    student_id
+  asc;
 -- using cross join   
